e8bb0b8618f0ec98641a2743fdc12862d183f944
Flavia <flaviamatta90@gmail.com>
2020-08-26 16:45:37 +0200

    finish

--- js/script.js
+++ js/script.js
@@ -3,14 +3,25 @@
 
 // 1 Scrivi un programma che stampi i numeri da 1 a 100
 
-var numeri = i
+var numeri = [];
 
 for (var i = 1; i <= 100; i++) {
-  var numeri = i;
-  console.log(numeri);
-}
+  numeri.push(i);
 
 // 2 Per i multipli di 3 stampi “Fizz” al posto del numero;
-if (i % 3 === 0) {
-  numeri.splice(3, 1, 'Fizz');
+  if (i % 3 === 0) {
+    numeri.splice(-1, 1, 'Fizz');
+  }
+
+  // 3 Per i multipli di 5 stampi “Fizz” al posto del numero;
+  if (i % 5 === 0) {
+    numeri.splice(-1, 1, 'Buzz');
+  }
+
+  // 4 Per i multipli di 3 e 5 stampi “Fizz” al posto del numero;
+  if (i % 15 === 0) {
+    numeri.splice(-1, 1, 'FizzBuzz');
+  }
+
 }
+console.log(numeri);
