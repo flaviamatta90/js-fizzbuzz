efb7494dcc46a8a67bc2ccff3df9737fbc98bd69
Flavia <flaviamatta90@gmail.com>
2020-08-26 14:43:54 +0200

    splice

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+
+// 1 Scrivi un programma che stampi i numeri da 1 a 100
+
+var numeri = i
+
+for (var i = 1; i <= 100; i++) {
+  var numeri = i;
+  console.log(numeri);
+}
+
+// 2 Per i multipli di 3 stampi “Fizz” al posto del numero;
+if (i % 3 === 0) {
+  numeri.splice(3, 1, 'Fizz');
+}
